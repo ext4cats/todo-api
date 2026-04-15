8fc8f5cbbdd1fbd3b431dbbdefd7c544e2bc336e
ext4cats <ext4cats@memoryconflux.com>
2026-04-15 00:00:23 -0300

    generate AddDataProtectionKeys migration

--- Todo.Api/Migrations/20260415025929_AddDataProtectionKeys.Designer.cs
+++ Todo.Api/Migrations/20260415025929_AddDataProtectionKeys.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Todo.Api;
@@ -11,9 +12,11 @@ using Todo.Api;
 namespace Todo.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260415025929_AddDataProtectionKeys")]
+    partial class AddDataProtectionKeys
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
